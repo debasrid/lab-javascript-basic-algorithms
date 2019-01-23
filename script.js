485d1c5a11d2b7a51f39a0776b4c179a049006ab
Debasri Dasgupta <46868809+debasrid@users.noreply.github.com>
2019-01-23 22:47:44 +0100

    Updated with exercise 10

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var hacker1 = "Iron";
-console.log("The driver's name is " + hacker1);
-var hacker2 = "iron";
+  console.log("The driver's name is " + hacker1);
+//var hacker2 = "iron";
 //console.log(hacker2);
-//var hacker2= prompt("Input a name? ");
-console.log("The navigator's name is  " + hacker2);
+var hacker2= prompt("Input a name? ");
+  console.log("The navigator's name is  " + hacker2);
 
 //Depending on which name is longer, print:
 if (hacker1.length > hacker2.length)
@@ -15,17 +15,19 @@ if (hacker1.length == hacker2.length)
 
 //Print all the characters of the driver's name, separated by a space and in capitals
 arrStr = [];
-for (var i = 0; i < hacker1.length; i++) {
-  arrStr.push(hacker1[i] + " ");
-}
-console.log(arrStr.join(" "));
+    for (var i = 0; i < hacker1.length; i++) {
+      arrStr.push(hacker1[i] + " ");
+    }
+      console.log(arrStr.join(""));
 
 //Print all the characters of the navigator's name, in reverse order. 
 x = [];
 for (var j = hacker2.length - 1; j >= 0; j--) {
-  x.push(hacker2[j].toUpperCase() + " ");
-}
-console.log(x.join(" "));
+      x.push(hacker2[j].toUpperCase()) ;
+      }
+    console.log(x.join(" "));
+
+
 
 //Depending on the lexicographic order of the strings, print: 
 //The driver's name goes first
@@ -51,20 +53,20 @@ function orderNames() {
 orderNames();
 
 //Ask the user for a new string and check if it's a Palindrome. 
-//var strInput="bb";
+
 var strInput = prompt("Input a sentence");
 var s = strInput.replace(/\s+|\,|\!|\.|\_|\-|\?|\(|\)|\/|\\/g, '').toLowerCase();
 var p = s.split("").reverse().join("");
-console.log(s);
-console.log(p);
-if (s === p) {
-  alert("The input is palindrome");
-}
-else {
-  alert("The input is not palindrome");
-}
+    console.log(s);
+    console.log(p);
+      if (s === p) {
+        alert("The input is palindrome");
+        }
+      else {
+        alert("The input is not palindrome");
+      }
 
-// ToDo:
+
 
 //Go to lorem ipsum generator and:
 
@@ -73,17 +75,24 @@ else {
 //Make your program count the number of times the latin word etappears    
 
 
-var lopStr = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate lectus odio, eget mattis lacus aliquet eget. Phasellus tincidunt sed nulla et vehicula. Donec nisl risus, fringilla at metus at, euismod ornare lectus. Ut eget nulla finibus eros fermentum placerat. Nam ac elit sodales, facilisis mauris sit amet, consequat libero. Praesent malesuada orci ut pharetra dapibus. Donec commodo neque id diam aliquam consectetur. Aenean vestibulum elit ac orci imperdiet, eu fermentum nisl semper. Sed rutrum bibendum facilisis. Duis vitae dolor nec ex elementum semper eget eu massa. Proin vitae faucibus lectus. Pellentesque convallis libero at est lobortis finibus. Cras euismod sapien ut vestibulum pellentesque. Maecenas mattis urna dui, porta iaculis purus porttitor id.Morbi sollicitudin a urna quis placerat. Ut sed mattis dolor. Mauris tristique dolor eget lectus sodales, vel lacinia diam convallis. Vestibulum fringilla dolor neque, id lobortis erat aliquet at. Etiam arcu mi, venenatis quis dui in, volutpat gravida ipsum. Pellentesque in ligula aliquam, ultricies ligula vel, convallis diam. Proin eget porttitor libero. Maecenas at euismod tellus, in imperdiet risus. Nam dignissim mattis lorem, id accumsan lectus gravida et. Aliquam dapibus dapibus nunc, a facilisis odio luctus at. Nam velit erat, sodales in dolor in, congue convallis nulla. Sed ac justo ac sem pellentesque semper. Nulla rutrum lacinia suscipit.Nam vehicula lorem ac fermentum gravida. Nam tincidunt dapibus bibendum. Nunc non finibus lorem. Nam sagittis ornare mi, id gravida nisi. Mauris posuere lacinia sem id volutpat. Etiam augue dolor, venenatis at felis sed, lacinia maximus risus. Morbi varius sollicitudin mollis. Aliquam suscipit nisi non consectetur convallis. Quisque tincidunt elit vitae mauris gravida rutrum.";
+var lopStr = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultrices lorem in vulputate semper. Donec nulla risus, tincidunt a nunc nec, ultrices vestibulum est. Morbi non gravida nisi, eu accumsan risus. Sed feugiat libero sit amet magna laoreet efficitur. Sed risus sapien, egestas in erat eu, bibendum facilisis turpis. Duis sit amet placerat lectus. Maecenas sodales eros id nulla tristique convallis non et nisl. Nam et sollicitudin felis. Ut et facilisis odio. Sed volutpat nisi elit, id consequat sapien pretium quis. Integer id lacinia augue, id feugiat ipsum. Duis vitae lacus lorem. Mauris interdum laoreet pulvinar. Integer ultrices nibh eu elementum condimentum. Donec bibendum tortor nibh, sed mattis tellus suscipit et. Sed ac suscipit sem, non maximus tortor. Integer bibendum hendrerit dolor a maximus. Donec mattis sagittis lorem ut pharetra. Sed consequat facilisis varius. Mauris egestas magna sit amet lorem mollis vestibulum. Integer sit amet tristique mi. Nullam libero justo, bibendum a felis eget, efficitur auctor nulla. Nunc egestas venenatis augue, id aliquam justo feugiat non. Nunc non orci nulla. Etiam nec metus leo. Morbi eget justo eleifend mi ornare euismod a aliquam risus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed malesuada sit amet enim sed interdum. Maecenas non felis sit amet neque aliquam venenatis. Etiam sit amet mauris vitae nunc rhoncus maximus quis non enim. Nulla sed bibendum odio, sed consectetur velit. Sed malesuada vehicula leo quis rhoncus. Ut et eros velit. Pellentesque velit enim, placerat in mi vitae, vestibulum tincidunt nisi. In non urna interdum, sodales lectus quis, semper nisi. Sed nec purus quis enim pulvinar euismod. Donec sit amet aliquam dolor. Praesent turpis enim, scelerisque id mauris ut, aliquet sodales elit. Ut nibh eros, volutpat dictum enim non, euismod tincidunt ex. Aenean placerat consectetur velit, viverra facilisis metus tristique non.";
 
 
 function wordcount(lopStr) {
   var c = lopStr.split(" ");
   var word = 0;
+  var count = 0;
+  
   for (var i = 0; i < c.length; i++) {
-    if (c[i] == " ") {
+    if (c[i] != "") {
+       if (c[i]=="et"){
+         count+=1;
+       }
       word += 1;
     }
+    
+    //console.log(c[i]);
   }
-  console.log("the total no of words: " + c.length);
+  console.log("the total no of words: " + word +" and total # of et:" + count);
 }
 wordcount(lopStr);
